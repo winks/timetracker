28c792a4435fca640fa6a6d32381c7ff22e54f7a
Florian Anderiasch <fa@art-core.org>
2018-03-08 00:18:17 +0100

    track some time

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -3,12 +3,155 @@
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
-    ui(new Ui::MainWindow)
+    ui(new Ui::MainWindow),
+    trayMenu(new QMenu),
+    iconDefault("../../resources/icon_default.png"),
+    iconGreen("../../resources/icon_green.png"),
+    isTracking(false),
+    timeElapsed(0)
 {
     ui->setupUi(this);
+    ui->label->setText("Elapsed time (s):");
+    ui->lcdNumber->setDigitCount(5);
+    ui->lcdNumber->setDecMode();
+
+    trayIcon = new QSystemTrayIcon(iconDefault);
+
+    aStart = trayMenu->addAction(QString("St&art"));
+    aStop  = trayMenu->addAction(QString("St&op"));
+    aSep1  = trayMenu->addSeparator();
+    aQuit  = trayMenu->addAction(QString("&Quit"));
+
+    aSep2  = new QAction;
+    aElapsed = new QAction;
+    aStop->setEnabled(false);
+
+    connect(aStart,   SIGNAL(triggered()), this, SLOT(startTracking()));
+    connect(aStop,    SIGNAL(triggered()), this, SLOT(stopTracking()));
+    connect(aQuit,    SIGNAL(triggered()), this, SLOT(close()));
+    connect(trayIcon, SIGNAL(activated(QSystemTrayIcon::ActivationReason)),
+            this, SLOT(toggleWindow(QSystemTrayIcon::ActivationReason)));
+
+    trayIcon->setContextMenu(trayMenu);
+
+    if (QSystemTrayIcon::isSystemTrayAvailable()) {
+        trayIcon->setIcon(iconDefault);
+        trayIcon->show();
+
+        if (QSystemTrayIcon::supportsMessages()) {
+            trayIcon->showMessage(QString("started timetracker"), QString(),
+                                 QSystemTrayIcon::Information, 500);
+        }
+    } else {
+        qDebug() << QString("no tray");
+    }
 }
 
 MainWindow::~MainWindow()
 {
+    delete aElapsed;
+    delete aQuit;
+    delete aSep1;
+    delete aSep2;
+    delete aStart;
+    delete aStop;
+    delete trayIcon;
+    delete trayMenu;
     delete ui;
+}
+
+void MainWindow::startTracking()
+{
+    if (isTracking) return;
+    isTracking = true;
+
+    QDateTime now = QDateTime::currentDateTimeUtc();
+    qDebug() << QString("startTracking @ %1").arg(now.toString());
+    lastStarted = now;
+    updateStuff();
+}
+
+void MainWindow::stopTracking()
+{
+    if (!isTracking) return;
+    isTracking = false;
+
+    QDateTime now = QDateTime::currentDateTimeUtc();
+    qDebug() << QString("stopTracking @ %1").arg(now.toString());
+    qint64 diff = lastStarted.msecsTo(now) / 1000;
+    timeElapsed += diff;
+    qDebug() << QString("stopTracking += %1 (%2)").arg(diff).arg(timeElapsed);
+    updateStuff();
+}
+
+void MainWindow::toggleWindow(QSystemTrayIcon::ActivationReason reason)
+{
+    if (reason == QSystemTrayIcon::Context) {
+        return;
+    } else if (reason != QSystemTrayIcon::Trigger) {
+        qDebug() << QString("received %1").arg(QString::number(reason));
+        return;
+    }
+    if(this->isVisible()) {
+        this->hide();
+    } else {
+        this->show();
+    }
+}
+
+void MainWindow::quitApp()
+{
+    this->close();
+}
+
+void MainWindow::updateStuff()
+{
+    // set icon based on current tracking status
+    if (isTracking) {
+        trayIcon->setIcon(iconGreen);
+        aStop->setEnabled(true);
+        aStart->setEnabled(false);
+    } else {
+        trayIcon->setIcon(iconDefault);
+        aStop->setEnabled(false);
+        aStart->setEnabled(true);
+    }
+    // update "elapsed" context menu item
+    if (timeElapsed > 0) {
+        QString elapsed = QString("elapsed: %1").arg(formatTime(timeElapsed));
+        aElapsed->setText(elapsed);
+        if (trayMenu->actions().size() == 4) {
+            trayMenu->insertAction(aSep1, aElapsed);
+            aSep2 = trayMenu->insertSeparator(aElapsed);
+        }
+    }
+    ui->lcdNumber->display(QString::number(timeElapsed));
+}
+
+QString MainWindow::formatTime(const qint64 & seconds_)
+{
+    QString rv;
+    qint64 seconds = seconds_;
+    qint64 tmp;
+    if (seconds >= SECS_PER_DAY) {
+        tmp = (seconds - (seconds % SECS_PER_DAY)) / SECS_PER_DAY;
+        rv += QString::number(tmp);
+        rv += " d";
+        seconds -= (tmp * SECS_PER_DAY);
+    }
+    if (seconds >= SECS_PER_HOUR) {
+        tmp = (seconds - (seconds % SECS_PER_HOUR)) / SECS_PER_HOUR;
+        rv += QString::number(tmp);
+        rv += " h";
+        seconds -= (tmp * SECS_PER_HOUR);
+    }
+    if (seconds >= SECS_PER_MIN) {
+        tmp = (seconds - (seconds % SECS_PER_MIN)) / SECS_PER_MIN;
+        rv += QString::number(tmp);
+        rv += "m";
+        seconds -= (tmp * SECS_PER_MIN);
+    }
+    rv += QString::number(seconds);
+    rv += "s";
+    return rv;
 }
--- mainwindow.h
+++ mainwindow.h
@@ -2,6 +2,14 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QSystemTrayIcon>
+#include <QIcon>
+#include <QDebug>
+#include <QDateTime>
+
+#define SECS_PER_DAY 86400
+#define SECS_PER_HOUR 3600
+#define SECS_PER_MIN 60
 
 namespace Ui {
 class MainWindow;
@@ -15,8 +23,32 @@ public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
 
+private slots:
+    void startTracking();
+    void stopTracking();
+    void toggleWindow(QSystemTrayIcon::ActivationReason reason);
+    void quitApp();
+
+private:
+    void updateStuff();
+    QString formatTime(const qint64 & seconds);
+
 private:
     Ui::MainWindow *ui;
+    QSystemTrayIcon * trayIcon;
+    QMenu * trayMenu;
+    QAction * aElapsed;
+    QAction * aQuit;
+    QAction * aSep1;
+    QAction * aSep2;
+    QAction * aStart;
+    QAction * aStop;
+
+    QIcon iconDefault;
+    QIcon iconGreen;
+    bool isTracking;
+    QDateTime lastStarted;
+    qint64 timeElapsed;
 };
 
 #endif // MAINWINDOW_H
